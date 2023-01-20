3dc3ad518bb3e4b6f49f52cf457a5eef90a227f5
Giles Edkins <edkins@gmail.com>
2023-01-20 16:21:43 -0500

    Tokneization and training

--- training/corpus.py
+++ training/corpus.py
@@ -1,6 +1,8 @@
 import nltk.corpus
 import random
 import re
+import more_itertools
+from typing import Callable
 
 re_word = re.compile(r'^[a-zA-Z0-9].*$')
 
@@ -12,10 +14,30 @@ def word_join(words: list[str]) -> str:
         word_spaces.append(w)
     return ''.join(word_spaces)
 
-def get_corpus(seed:int, n:int):
+# Returns a list of batches
+# Each batch is of length batch_size and is a list of windows
+# Each window is of length window_size and is a list of tokens (integers)
+def get_corpus(seed:int, n:int, train:bool, batch_size:int, window_size:int, tokenizer:Callable) -> list[list[list[int]]]:
     c = nltk.corpus.brown
     s = c.sents()
+    cutoff = int(len(s) * 0.8)
+    if train:
+        s = s[:cutoff]
+    else:
+        s = s[cutoff:]
     random.seed(seed)
     randoms = random.sample(range(len(s)), k=n)
-    return [word_join(s[r]) for r in randoms]
+    
+    result = []
+    for r in randoms:
+        tokens = []
+        i = 0
+        while len(tokens) < window_size:
+            sentence = word_join(s[r + i % len(s)])
+            tokens += tokenizer.encode(sentence)
+            i += 1
+        tokens = tokens[:window_size]
+        result.append(tokens)
+    
+    return more_itertools.batched(result, batch_size)
 
--- training/training.py
+++ training/training.py
@@ -1,4 +1,6 @@
 from transformer_lens import HookedTransformer, HookedTransformerConfig
+import tqdm
+import torch
 
 from corpus import get_corpus
 
@@ -9,6 +11,13 @@ seed = 999
 
 #https://huggingface.co/transformers/v3.0.2/model_doc/auto.html#autotokenizer
 tokenizer_name = 'gpt2'
+
+num_epochs = 20
+n_corpus = 1000
+
+device = 'cuda'
+
+######
 
 cfg = HookedTransformerConfig(
     n_layers = n_layers,
@@ -21,11 +30,30 @@ cfg = HookedTransformerConfig(
     tokenizer_name=tokenizer_name,
     n_ctx = n_ctx,
     init_weights=True,
-    device="cuda",
+    device = device,
     seed = seed,
 )
 
 model = HookedTransformer(cfg)
-for s in get_corpus(seed, 100):
-    print(s)
+d_vocab = model.cfg.d_vocab
+
+optimizer = torch.optim.Adam(model.parameters())
+loss_fn = torch.nn.CrossEntropyLoss()
+
+for epoch in tqdm.tqdm(range(num_epochs)):
+    loss_sum = torch.zeros(()).to(device)
+    for sentences in get_corpus(seed, n_corpus, train=True, batch_size=64, window_size=n_ctx+1, tokenizer=model.tokenizer):
+        stensor = torch.tensor(sentences).to(device)
+        num_things = stensor.shape[0] * n_ctx
+        nexts = stensor[:,1:].reshape(num_things)
+        inputs = stensor[:,:-1]
+        
+        logits = model(inputs).reshape(num_things, d_vocab)
+        loss = loss_fn(logits, nexts)
+        loss.backward()
+        loss_sum += loss.detach()
+        
+        optimizer.step()
+        optimizer.zero_grad()
+    print(f'Epoch {epoch}: train loss = {loss_sum.item()}')
 
