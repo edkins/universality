02e544d953646c6e2090db66da0cf03ff008b266
Giles Edkins <edkins@gmail.com>
2023-01-20 14:50:08 -0500

    Create a (blank) TransformerLens model

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # universality
+
+# Training the model
+pip install git+https://github.com/neelnanda-io/TransformerLens
+python3 training/training.py
+
---
+++ training/training.py
@@ -0,0 +1,26 @@
+from transformer_lens import HookedTransformer, HookedTransformerConfig
+
+n_layers = 3
+n_heads = 4
+n_ctx = 16  # The maximum sequence length
+
+#https://huggingface.co/transformers/v3.0.2/model_doc/auto.html#autotokenizer
+tokenizer_name = 'gpt2'
+
+cfg = HookedTransformerConfig(
+    n_layers = n_layers,
+    n_heads = n_heads,
+    d_model = 128,
+    d_head = 32,
+    d_mlp = 512,
+    act_fn = "relu",
+    normalization_type=None,
+    tokenizer_name=tokenizer_name,
+    n_ctx = n_ctx,
+    init_weights=True,
+    device="cuda",
+    seed = 999,
+)
+
+model = HookedTransformer(cfg)
+
